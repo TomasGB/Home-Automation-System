f4b775f674b0d551f0d5246b966d0963e2ba47b8
Tomas Gomez Bermudez <g.btomas@hotmail.com>
2025-12-17 16:38:11 -0300

    fixed addDeviceModal bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ const App = () => {
       
       {showAddDevice && (
         <div style={{
-          position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          position: 'fixed', top: 0, left: 0, width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: 9999,
         }}>
         <AddDeviceModal
           device={editingDevice}
